5690fa2dd35bfe6996c86fb02b07d11c2223a0a9
Grube199 <31861383+Grube199@users.noreply.github.com>
2018-11-18 21:51:39 -0600

    Changed titles
    Task topic title: How to
    Reference title: verb to noun
    Concept title: Question to noun

--- oxygen-webhelp/app/search/index/keywords.js
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -1,13 +1,13 @@
-define(function() {var keywords=[{w:"Why",p:["p0"]},{w:"addictions?",p:["p0"]},{w:"How",p:["p1"]},{w:"to",p:["p1"]},{w:"communicate",p:["p1"]},{w:"Enabling",p:["p2"]},{w:"Intervention",p:["p3"]},{w:"of",p:["p3"]},{w:"an",p:["p3"]},{w:"addicted",p:["p3"]},{w:"person",p:["p3"]},{w:"Prayer",p:["p4","p5"]},{w:"ideas",p:["p5"]},{w:"Resources",p:["p6"]},{w:"Prodigals",p:["p7"]}];
+define(function() {var keywords=[{w:"Addictions",p:["p0"]},{w:"How",p:["p1","p3"]},{w:"to",p:["p1","p3"]},{w:"communicate",p:["p1"]},{w:"Communication:",p:["p2"]},{w:"enabling",p:["p2"]},{w:"conduct",p:["p3"]},{w:"an",p:["p3"]},{w:"intervention",p:["p3"]},{w:"Prayer",p:["p4","p5"]},{w:"ideas",p:["p5"]},{w:"Resources",p:["p6"]},{w:"Prodigals",p:["p7"]}];
 var ph={};
-ph["p0"]=[0, 1];
-ph["p1"]=[2, 3, 4];
-ph["p2"]=[5];
-ph["p3"]=[6, 7, 8, 9, 10];
-ph["p4"]=[11];
-ph["p5"]=[11, 12];
-ph["p6"]=[13];
-ph["p7"]=[14];
+ph["p0"]=[0];
+ph["p1"]=[1, 2, 3];
+ph["p2"]=[4, 5];
+ph["p3"]=[1, 2, 6, 7, 8];
+ph["p4"]=[9];
+ph["p5"]=[9, 10];
+ph["p6"]=[11];
+ph["p7"]=[12];
      return {
          keywords: keywords,
          ph: ph
